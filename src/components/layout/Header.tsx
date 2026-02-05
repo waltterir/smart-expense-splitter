41881fbc42f7610b15295d2e266b5b18f391273c
waltterir <riihimakiwaltteri@gmail.com>
2026-02-05 14:22:11 +0200

    added header, container, homepage stucture

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header className="header">
+      <div className="headerInner">
+        <div>
+          <h1 className="title">Smart Expense Splitter</h1>
+          <p className="subtitle">runko: People / Expenses / Summary</p>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+export function HomePage() {
+  return (
+    <main className="layout">
+      <div className="leftCol">
+        <section className="stack"></section>
+      </div>
+      <div className="rightCol"></div>
+    </main>
+  );
+}
