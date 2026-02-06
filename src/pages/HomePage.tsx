61792de29707de48d4b95fd4ab183b13cda94bf1
waltterir <riihimakiwaltteri@gmail.com>
2026-02-06 14:42:11 +0200

    Added description and amount to expense form

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,7 @@ export function HomePage() {
   const [paidById, setPaidById] = useState<number | null>(null);
   const [participantsId, setParticipantsId] = useState<number[]>([]);
 
+  // listään henkilö listaan
   const addPerson = () => {
     const id = Date.now();
     const name = newName;
@@ -22,6 +23,7 @@ export function HomePage() {
     setPeople((prev) => [...prev, newPerson]);
   };
 
+  // poistetaan henkilö listasta
   const removePerson = (id: number) => {
     setPeople((prev) => prev.filter((person: Person) => person.id !== id));
   };
@@ -37,7 +39,7 @@ export function HomePage() {
               value={newName}
               onChange={(e) => setNewName(e.target.value)}
               type="text"
-              placeholder="Kirjoita nimi"
+              placeholder="Type a name: "
             />
             <button className="ml-5" onClick={addPerson}>
               Add
@@ -45,7 +47,7 @@ export function HomePage() {
           </div>
           {/* Lista ihmisistä */}
           <div>
-            <p>Lisätyt ihmiset: </p>
+            <p>Added people: </p>
             <ul>
               {people.length === 0 ? (
                 <li>No people added yet</li>
@@ -67,8 +69,34 @@ export function HomePage() {
         </section>
         <section className="panel">
           <h2 className="panelTitle">Expenses</h2>
-          <p>Add expenses tänne</p>
+          <div className="mb-5">
+            <p>Description</p>
+            <input
+              className="bg-white text-black"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              type="text"
+              name="description"
+              id="descriptionId"
+              placeholder="Give Description:"
+            />
+          </div>
+
+          <div className="mb-5">
+            <h2>Amount</h2>
+            <input
+              className="bg-white text-black"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              type="number"
+              name="amount"
+              id="amountId"
+              placeholder="Enter amount: "
+            />
+          </div>
+
           <select
+            className="bg-gray-600 text-white"
             name="paidById"
             id="expenses"
             value={paidById ?? ""}
@@ -81,7 +109,7 @@ export function HomePage() {
               }
             }}
           >
-            <option value="">Valitse Maksaja: </option>
+            <option value="">Choose payer:</option>
             {people.map((person) => (
               <option value={person.id} key={person.id}>
                 {person.id}
@@ -89,6 +117,32 @@ export function HomePage() {
               </option>
             ))}
           </select>
+          <div className="mt-10">
+            <p>Participants</p>
+            {people.map((person) => (
+              <ul>
+                <li key={person.id}>
+                  {person.name}
+                  <input
+                    type="checkbox"
+                    checked={participantsId.includes(person.id)}
+                    onChange={() => {
+                      const isSelected = participantsId.includes(person.id);
+                      if (isSelected) {
+                        setParticipantsId((prev) =>
+                          prev.filter(
+                            (participantId) => participantId !== person.id,
+                          ),
+                        );
+                      } else {
+                        setParticipantsId((prev) => [...prev, person.id]);
+                      }
+                    }}
+                  />
+                </li>
+              </ul>
+            ))}
+          </div>
         </section>
       </div>
       <div className="rightCol">
